e5cff7705dfa1847dc633d10348952f4713e4556
bonnina <37539513+bonnina@users.noreply.github.com>
2019-12-22 22:55:56 +0200

    Add project files.

---
+++ JSON_editor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JSON_editor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
